763d507be95a7299813737dd256d0309f5488d9c
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2020-11-28 18:19:33 -0800

    [UPDATE] modify vector op function names for clarity

--- structures/objects.py
+++ structures/objects.py
@@ -4,27 +4,27 @@ import warnings
 warnings.simplefilter(action="ignore", category=FutureWarning)
 import tensorflow as tf
 
-def joinV(vectorA, vectorB):
+def vectorial_join(vectorA, vectorB):
     """ Custom join via vectorized concatenation. """
     return tf.concat([vectorA, vectorB], 0)
 
-def labeler_(vectors, batch_size):
+def vectorial_labeler(vectors, batch_size):
     """ Custom helper for labeling entries based on batch size. """
     if vectors is not None:
         return tf.slice(vectors, [0, 0], [batch_size, -1])
     else:
         return vectors
     
-def unlabeler_(vectors, batch_size):
+def vectorial_unlabeler(vectors, batch_size):
     """ Custom helper for unlabeling entries based on batch size. """
     if vectors is not None:
         return tf.slice(vectors, [batch_size, 0], [-1, -1])
     else:
         return vectors
     
-def splitV(vectors):
+def vectorial_split(vectors):
     """ Custom split via helper functions performing vectorized slicing. """
-    return (labeler_(vectors), unlabeler_(vectors))
+    return (vectorial_labeler(vectors), vectorial_unlabeler(vectors))
 
 class DatasetReport(object):
     """ write summary here """
